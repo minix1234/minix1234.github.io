9e1f84db9f603d386e25c6b97755dcee17df95a0
minix1234 <minix1234@outlook.com>
2020-04-09 10:11:59 -0400

    scriots for content generation

---
+++ pelican/content/interactive.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'  #time_series_19-covid-Confirmed.csv'#
+dfcases = pd.read_csv(url,index_col=0,parse_dates=[0])
+dfcases = dfcases.drop(columns=['Lat','Long'])
+threshold = 10000  #used to remove countries in earlier infections from plots
+
+dfcases.at['Quebec','4/4/20'] = 6997 # fixes an issue with the numbers from WHO
+
+df = dfcases.groupby('Country/Region').sum()
+#df = df.T
+df = df.reset_index()
+#df = df.drop([0, 1])
+#df['index'] = df['index'].astype('datetime64')
+#df = df.set_index('index')
+
+
+df = df.rename(columns={'Country/Region': 'date'})
+df = df.T
+df.columns = df.iloc[0]
+df = df.drop(df.index[0])
+df = df.reset_index()
+df = df.rename(columns={'index': 'dates'})
+df['dates'] = df['dates'].astype('datetime64')
+df = df.set_index('dates')
+df = df.apply(pd.to_numeric)
+
+dfmax = pd.DataFrame(df.max(),columns=['max'] )
+dfmax = dfmax.reset_index()
+dfmax = dfmax.rename(columns={'date': 'country'})
+
+dfmax = dfmax[dfmax['max'] > threshold]
+llist = dfmax['country'].values.tolist()
+llist = llist + ['Japan']
+df1 = df[llist]
+
+dfgt = pd.DataFrame()
+dflt = pd.DataFrame()
+shift = {}
+
+for country in llist:
+    #Check country data for if the value is greater than 100
+    dfgt[country] = df1[country]>=100
+    # Determine the number of days to shift backwards to day zero.
+    sh = pd.to_datetime('2020-01-22') - dfgt[country].idxmax() #idxmax() -> find the first instance of True in the above Boolean list
+    df1[country] = df1[country].shift(sh.days)
+    
+    #if country in poplist:
+    #    try:
+    #        num = pop['Population'].loc[pop['Country'] == country].values
+    #        df[country] = df[country]/num[0]*100
+    #    except:
+    #        pass
+
+
+df1['China'] = df1['China'].shift(4)
+
+
+df1.index = pd.to_timedelta(df.index - pd.to_datetime('2020-01-22')).days
+
+dfmelt = df1.reset_index()
+dfmelt = dfmelt.melt(id_vars=['dates'])
+dfmelt = dfmelt.rename(columns={'date': 'country'})
+dfmelt = dfmelt.rename(columns={'value': 'cases'})
+#dfmelt = dfmelt.drop(columns=['dates'])
+dfmelt = dfmelt.set_index('dates')
+dfmelt['country'] = dfmelt['country'].astype('category')
+#dfmelt['cases'] = dfmelt['cases'].astype('int32') # fails due to nan
+gbt = dfmelt.groupby(dfmelt.index).sum()
+
+fig = px.line(dfmelt, x=dfmelt.index, y="cases",color='country')#,log_y=True)
+fig.update_layout(
+    title="Aligned Covid-19 Active Cases Data",
+    xaxis_title="Days Since 100th Active Case",
+    yaxis_title="Number of Active Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#fig.show()
+fig.write_html("infections_aligned.html")
+
+figlog = px.line(dfmelt, x=dfmelt.index, y="cases",color='country',log_y=True)
+figlog.update_layout(
+    title="Aligned Covid-19 Active Cases Data",
+    xaxis_title="Days Since 100th Active Case",
+    yaxis_title="Number of Active Cases [Log Scale]",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#figlog.show()
+figlog.write_html("infections_logscal.html")
+
+df2 = df1.diff()
+dfmelt2 = df2.reset_index()
+dfmelt2 = dfmelt2.melt(id_vars=['dates'])
+dfmelt2 = dfmelt2.rename(columns={'date': 'country'})
+dfmelt2 = dfmelt2.rename(columns={'value': 'cases'})
+#dfmelt = dfmelt.drop(columns=['dates'])
+dfmelt2 = dfmelt2.set_index('dates')
+dfmelt2['country'] = dfmelt2['country'].astype('category')
+#dfmelt['cases'] = dfmelt['cases'].astype('int32') # fails due to nan
+#dfmelt2.tail()
+
+figdif = px.line(dfmelt2, x=dfmelt2.index, y="cases", color='country')
+figdif.update_layout(
+    title="New Daily Cases",
+    xaxis_title="Days Since 100th Active Case",
+    yaxis_title="Daily Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#figdif.show()
+figdif.write_html("differences.html")
+
+pop = pd.read_csv('population.csv')
+poplist = pop['Country'].to_list()
+
+dfpop = pd.DataFrame()
+
+for country in llist:
+    # Check is country name from Github is also in the population CSV
+    if country in poplist:
+        try:
+            num = pop['Population'].loc[pop['Country'] == country].values #find the population data, comes out as array
+            dfpop[country] = df1[country]/num[0]*100 #convert the df cases values from absolute to percetnage of country population
+        except:
+            pass
+
+dfpctmelt = dfpop.reset_index()
+dfpctmelt = dfpctmelt.melt(id_vars=['dates'])
+
+dfpctmelt = dfpctmelt.rename(columns={'variable': 'country'})
+dfpctmelt = dfpctmelt.rename(columns={'value': 'percent'})
+dfpctmelt = dfpctmelt.set_index('dates')
+dfpctmelt['country'] = dfpctmelt['country'].astype('category')
+
+popfig = px.line(dfpctmelt, x=dfpctmelt.index, y="percent",color='country',log_y=True)
+popfig.update_layout(
+    title="Aligned Covid-19 Active Cases Data",
+    xaxis_title="Days Since 100th Active Case",
+    yaxis_title="Percent of Country Infected",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#popfig.show()
+popfig.write_html("percentage_aligned.html")
---
+++ pelican/content/ontario.py
@@ -0,0 +1,150 @@
+url = 'https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv'
+urlstatus = 'https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv'
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+dfcases = pd.read_csv(url,index_col=1,parse_dates=[1])
+dfcases = dfcases.drop(columns=['Reporting_PHU_Website','Reporting_PHU_Latitude','Reporting_PHU_Longitude','Reporting_PHU_City','Reporting_PHU_Address','Reporting_PHU_Postal_Code'])
+
+df2 = dfcases.groupby(['Age_Group','OUTCOME1']).count()
+df2 = df2.drop(columns=['CLIENT_GENDER','CASE_ACQUISITIONINFO','Reporting_PHU'])
+df2 = df2.reset_index()#.to_csv('ontario.csv')
+df2 = df2.rename(columns={'OUTCOME1': 'Outcome'})
+df2 = df2.rename(columns={'ROW_ID': 'Cases'})
+
+figage = px.bar(df2, x="Age_Group", y="Cases", color='Outcome')
+figage.update_layout(
+    title="Ontario Covid-19 Cases by Age Group and Case Outcome",
+    xaxis_title="Age Grouping",
+    yaxis_title="Number of Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#figage.show()
+figage.write_html("Ontariobarage.html")
+
+df3 = dfcases.groupby(['Reporting_PHU','OUTCOME1']).count()
+df3 = df3.drop(columns=['CLIENT_GENDER','CASE_ACQUISITIONINFO','Age_Group'])
+df3 = df3.reset_index()#.to_csv('ontario.csv')
+df3 = df3.rename(columns={'OUTCOME1': 'Outcome'})
+df3 = df3.rename(columns={'ROW_ID': 'Cases'})
+
+figPHU = px.bar(df3, x="Reporting_PHU", y="Cases", color='Outcome')
+figPHU.update_layout(
+    title="Ontario Covid-19 Cases by Reportin Public Health Unit and Case Outcome",
+    xaxis_title="",
+    yaxis_title="Number of Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=12,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#figPHU.show()
+figPHU.write_html("OntarioPHU.html")
+
+dfstatus = pd.read_csv(urlstatus,index_col=0,parse_dates=[0])
+dfstatus = dfstatus.astype(float)
+
+df1 = dfstatus
+df1 = df1.apply(pd.to_numeric)
+dfmelt = df1.reset_index()
+dfmelt = dfmelt.melt(id_vars=['Reported Date'])
+dfmelt = dfmelt.set_index('Reported Date')
+dfmelt['variable'] = dfmelt['variable'].astype('category')
+
+fig = px.line(dfmelt, x=dfmelt.index, y="value",color='variable')#,log_y=True)
+fig.update_layout(
+    title="Ontario Covid-19 Data",
+    xaxis_title="Dates",
+    yaxis_title="Values",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#fig.show()
+fig.write_html("Ontario.html")
+
+
+
+df3 = dfcases.reset_index()
+df4 = pd.DataFrame()
+df4['CASES'] = df3.groupby(['ACCURATE_EPISODE_DATE','OUTCOME1'])['ROW_ID'].count()
+df4 = df4.reset_index()
+df4['OUTCOME1'] = df4['OUTCOME1'].astype('category')
+
+
+
+fig = px.bar(df4, x="ACCURATE_EPISODE_DATE", y="CASES", color='OUTCOME1')
+fig.update_layout(
+    title="Ontario Covid-19 Data by Presentation Date",
+    xaxis_title="Dates",
+    yaxis_title="Number of Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#fig.show()
+fig.write_html("Ontariobar.html")
+
+
+dffatal = dfcases[dfcases['OUTCOME1'] == 'Fatal'].groupby(['Age_Group']).count()
+dffatal = dffatal.rename(columns={'ROW_ID': 'Number of Dead'})
+dffatal = dffatal.reset_index()
+
+
+figpie = px.pie(dffatal, values='Number of Dead', names='Age_Group', title='')
+figpie.update_layout(
+    title="Ontario Covid-19 Deaths",
+    font=dict(
+        family="Courier New, monospace",
+        size=16,
+        color="#7f7f7f"
+    ),
+    legend=dict(
+        font=dict(
+            family="sans-serif",
+            size=12,
+            color="black"
+        )
+    )
+)
+#figpie.show()
+figpie.write_html("Ontariopie.html")
