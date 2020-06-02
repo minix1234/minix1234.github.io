8479f270e47781a00edd302db56ce182c232cc32
minix1234 <minix1234@outlook.com>
2020-06-01 23:14:31 -0400

    test

--- pelican/content/ontario.py
+++ pelican/content/ontario.py
@@ -64,6 +64,7 @@ figPHU.write_html("OntarioPHU.html")
 
 dfstatus = pd.read_csv(urlstatus,index_col=0,parse_dates=[0])
 dfstatus = dfstatus.astype(float)
+dfstatus = dfstatus[dfstatus.columns[0:13]]
 dfstatus.columns = ['Confirmed Negative','Presumptive Negative','Presumtive Positive','Confirmed Positive','Resolved','Deaths','Total Cases','Approved for Testing','Tests Completed','Under Investigation','Hospitalized','In ICU','On Venilator']
 
 df1 = dfstatus
